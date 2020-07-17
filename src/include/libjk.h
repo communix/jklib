8f8d59b07e47bdd27e3913ee881d284d4d56e38d
Jay Kim <world.iot.blog@gmail.com>
2020-07-16 21:58:42 -0700

    Making directory structure and skeleton code.  - #2

---
+++ src/include/libjk.h
@@ -0,0 +1,8 @@
+/**
+ * @file libjk.h
+ * @brief functions in the libjk (JK library)
+ * @author Jay Kim
+ * @date 07/24/2020
+ */
+
+void empty_function(void);
---
+++ src/jk_common.c
@@ -0,0 +1,10 @@
+/**
+ * @file jk_common.c
+ * @brief common functions
+ */
+#include <libjk.h>
+
+void empty_function(void)
+{
+    
+}
