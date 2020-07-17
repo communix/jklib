7f4f658a3c58f5e1887570f4eb3fb65e5637df57
Jay Kim <changjae.kim@gmail.com>
2020-07-17 00:29:06 -0700

    Adding jk_type.h  - #5

---
+++ src/include/jk_type.h
@@ -0,0 +1,21 @@
+/**
+ * @file jk_type.h
+ * @brief define a new type to support multi platform
+ */
+#ifndef __JK_TYPE_H
+#define __JK_TYPE_H
+
+/**
+ * @typedef integer type for 64bit windows support
+ * @{
+ */
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int int64_t;
+typedef unsigned long long int uint64_t;
+/** @} */
+#endif //__JK_TYPE_H
