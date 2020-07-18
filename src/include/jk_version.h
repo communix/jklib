d50020501a2f2fff506686f0b2facc740465ca3d
Jay Kim <changjae.kim@gmail.com>
2020-07-17 23:39:01 -0700

    jk_get_version function with unit test  - #3

---
+++ src/include/jk_version.h
@@ -0,0 +1,18 @@
+/**
+ * @file jk_version.h
+ * @brief JKLib version information
+ * @defgroup
+ * @{
+ */
+
+#ifndef __JK_VERSION_H
+#define __JK_VERSION_H
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+#define JKLIB_VERSION_STRING "0.0.1"
+#define JKLIB_VERSION ((0<<24) | (0<<16) | 1)
+
+#endif
+/** @} */
--- src/unit_test/jklib_unit_test.c
+++ src/unit_test/jklib_unit_test.c
@@ -2,13 +2,18 @@
  * @file jk_unit_test.c
  * @brief unit test of the libjk
  */
-#include <libjk.h>
 #include <jk_type.h>
+#include <libjk.h>
 #include <unity.h>
 
+#define JKLIB_VERSION           0x00000001  //00.00.0001
 void setUp (void) {} /* Is run before every test, put unit init calls here. */
 void tearDown (void) {} /* Is run after every test, put unit clean-up calls here. */
 
+void test_jklib_version(void)
+{
+    TEST_ASSERT_EQUAL_UINT32_MESSAGE(JKLIB_VERSION, jk_get_version(), "JKLIB version is not matched");
+}
 void test_integer_type_size(void)
 {
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, sizeof(int8_t), "int8_t is not 1 byte");
@@ -20,9 +25,11 @@ void test_integer_type_size(void)
     TEST_ASSERT_EQUAL_INT_MESSAGE(8, sizeof(int64_t), "int64_t is not 8 byte");
     TEST_ASSERT_EQUAL_INT_MESSAGE(8, sizeof(uint64_t), "uint64_t is not 8 byte");
 }
+
 int main()
 {
     UNITY_BEGIN();
+    RUN_TEST(test_jklib_version);
     RUN_TEST(test_integer_type_size);
     UNITY_END();
     return 1;
