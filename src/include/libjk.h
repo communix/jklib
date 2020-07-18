200a7ce93ae4b89f92257192476a6e56b38a3b47
Jay Kim <changjae.kim@gmail.com>
2020-07-18 02:16:41 -0700

    two data swap function  - #1

--- src/include/libjk.h
+++ src/include/libjk.h
@@ -11,3 +11,11 @@
  * @return version of the JKLib. [31:24] Major [23:16] Minor [15:0] Patch version
  */
 uint32_t jk_get_version(void);
+
+/**
+ * @name jk_swap_int32
+ * @brief swap two data values
+ * @param a int32 address - first data
+ * @param b int32 address - second data
+ */
+void jk_swap_int32(int32_t *a, int32_t *b);
