ca187d14efd5626b5bef9683ddad8ee2ac88178f
Jay Kim <changjae.kim@gmail.com>
2020-08-16 22:09:17 -0700

    Linux build env  - Ubuntu 20.04

--- src/include/jk_type.h
+++ src/include/jk_type.h
@@ -15,8 +15,9 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
-typedef signed long long int int64_t;
-typedef unsigned long long int uint64_t;
+// conflict at linux build env.
+//typedef signed long long int int64_t;
+//typedef unsigned long long int uint64_t;
 /** @} */
 
 /**
